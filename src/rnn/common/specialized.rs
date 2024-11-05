4f93c3035e6b645f34d2aca69dbe43cb98cb9c64
Sergey Reshetnikov <shaman@simmirra.com>
2024-11-05 11:54:24 +0300

    Added doc for get_spec_type method

--- src/rnn/common/specialized.rs
+++ src/rnn/common/specialized.rs
@@ -2,5 +2,6 @@ use super::spec_type::SpecificationType;
 
 /// An entity that has a specific type and can show it to other.
 pub trait Specialized {
+    /// Returns SpecificationType to differ concrete component from other
     fn get_spec_type(&self) -> SpecificationType;
 }
