43a101a38d16df225451f12bc3d268f39c6857a0
Sergey Reshetnikov <shaman@simmirra.com>
2024-11-05 11:52:29 +0300

    added details to code documentation

--- src/rnn/common/specialized.rs
+++ src/rnn/common/specialized.rs
@@ -1,6 +1,6 @@
 use super::spec_type::SpecificationType;
 
-/// An entity that has a specific type.
+/// An entity that has a specific type and can show it to other.
 pub trait Specialized {
     fn get_spec_type(&self) -> SpecificationType;
 }
