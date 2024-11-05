937e244437251ebab0f291b83d43f004edf61633
Sergey Reshetnikov <shaman@simmirra.com>
2024-11-05 12:02:01 +0300

    remove Specialized dependency from Acceptor

--- src/rnn/common/acceptor.rs
+++ src/rnn/common/acceptor.rs
@@ -1,6 +1,3 @@
-use super::specialized::Specialized;
-
 /// An entity that can receive a signal emitted from an emitter or input terminator.
 /// Usually it can be represented as Synapse or as OutputTerminator.
-/// Should have SpecificationType to differ from other components.
-pub trait Acceptor: Specialized {}
+pub trait Acceptor {}
