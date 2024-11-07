9f0ddab29bd4804357854d9cf2fcf3e5bc434d4f
Sergey Reshetnikov <shaman@simmirra.com>
2024-11-07 18:46:55 +0300

    up minor version

--- src/rnn/common/connectable.rs
+++ src/rnn/common/connectable.rs
@@ -1,5 +1,9 @@
 /// An entity can connect to or disconnect from another connected entity.
 pub trait Connectable {
+    /// Connect to other component by its id
+    /// This component should be in owner container except when axon tried connect to synapse from another neuron
     fn connect(&mut self, _party_id: &str) {}
+
+    /// Disconnect from connected component by its id
     fn disconnect(&mut self, _party_id: &str) {}
 }
