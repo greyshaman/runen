faf2932fdc2ce69e5b84230bdf56949b7454d0bc
Sergey Reshetnikov <shaman@simmirra.com>
2025-03-11 14:35:33 +0300

    Update Status to use new signal structure

--- src/rnn/common/status.rs
+++ src/rnn/common/status.rs
@@ -1,10 +1,15 @@
+use std::fmt::Debug;
+
 use chrono::{DateTime, Utc};
 
-use super::signal::{Signal, Weight};
+use super::{arithmetic::Arithmetic, signal::Signal};
 
 /// Current neuron state.
 #[derive(Debug, Clone)]
-pub struct NeuronInfo {
+pub struct NeuronInfo<S>
+where
+    S: Arithmetic,
+{
     /// The status of the neuron at a time.
     pub timestamp: DateTime<Utc>,
 
@@ -27,17 +32,20 @@ pub struct NeuronInfo {
     pub hit_count: u64,
 
     /// The accumulator value
-    pub accumulator: Weight,
+    pub accumulator: S,
 
     /// The number of active receivers.
     pub receiver_count: usize,
 
     /// The sum of dendrites weight
-    pub total_weight: Weight,
+    pub total_weight: S,
 }
 
 #[derive(Debug, Clone)]
-pub struct PortInfo {
+pub struct PortInfo<S>
+where
+    S: Arithmetic,
+{
     /// The port status at a time.
     pub timestamp: DateTime<Utc>,
 
@@ -48,12 +56,15 @@ pub struct PortInfo {
     pub hit_count: u64,
 
     /// Keep last signal value
-    pub recent_signal: Signal,
+    pub recent_signal_value: S,
 }
 
 #[derive(Debug, Clone)]
-pub enum Status {
-    Neuron(NeuronInfo),
+pub enum Status<S>
+where
+    S: Arithmetic,
+{
+    Neuron(NeuronInfo<S>),
 
-    Port(PortInfo),
+    Port(PortInfo<S>),
 }
