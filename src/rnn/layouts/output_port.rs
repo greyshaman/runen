6d1b4a5a63a65b9fb8212112a38fa73216798c12
Sergey Reshetnikov <shaman@simmirra.com>
2025-03-18 13:08:03 +0300

    reimplemented output port

--- src/rnn/layouts/output_port.rs
+++ src/rnn/layouts/output_port.rs
@@ -1,24 +1,11 @@
-use std::{
-    error::Error,
-    pin::Pin,
-    sync::{Arc, mpsc::Receiver},
-};
+use std::{error::Error, pin::Pin, sync::{atomic::{AtomicUsize, Ordering}, Arc}, task::Poll};
 
-use tokio::sync::{RwLock, broadcast, mpsc};
+use tokio::sync::broadcast;
 use tokio_stream::Stream;
 
 use crate::rnn::common::{arithmetic::Arithmetic, rnn_error::RnnError, signal::Signal};
 
-pub trait Subscriber<S>: Stream<Item = S>
-where
-    S: Arithmetic,
-{
-    fn set_receiver(&mut self, receiver: mpsc::Receiver<S>) -> Result<(), Box<dyn Error>>;
-}
-
-/// The OutputPort receive signal from neuron and should emit event into external world
-/// Выходной порт может иметь подписчиков которые подписываются на изменение данных
-/// и выполняет функции замыкания подписчиков
+/// The output port receives a signal from the neuron and emits an event into the external world.
 pub struct OutputPort<S>
 where
     S: Arithmetic,
@@ -27,156 +14,237 @@ where
     id: Arc<String>,
 
     /// signal hits counter
-    signal_hits_counter: Arc<RwLock<usize>>,
+    signal_hits_counter: Arc<AtomicUsize>,
 
-    /// signal receive
-    inner_receiver: Option<Arc<RwLock<broadcast::Receiver<Signal<S>>>>>,
-
-    /// output channel
-    outgoing_sender: Arc<RwLock<Option<Arc<mpsc::Sender<S>>>>>,
+    /// signal receiver
+    receiver: Option<broadcast::Receiver<Signal<S>>>,
 }
 
 impl<S> OutputPort<S>
 where
     S: Arithmetic,
 {
+    /// Constructs a new output port
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use librunen::rnn::layouts::output_port::OutputPort;
+    ///
+    /// let port: OutputPort<u8> = OutputPort::new(0);
+    /// ```
+    ///
+    /// ```
+    /// use librunen::rnn::layouts::output_port::OutputPort;
+    ///
+    /// let port = OutputPort::<u8>::new(0);
+    /// ```
     pub fn new(id: usize) -> Self {
         OutputPort {
             id: Arc::new(format!("O_{}", id)),
-            signal_hits_counter: Arc::new(RwLock::new(0)),
-            inner_receiver: None,
-            outgoing_sender: Arc::new(RwLock::new(None)),
+            signal_hits_counter: Arc::new(AtomicUsize::new(0)),
+            receiver: None,
         }
     }
 
-    pub async fn subscribe(
-        &self,
-        subscriber: Arc<RwLock<dyn Subscriber<S>>>,
-        outgoing_buffer_size: usize,
-    ) -> Result<(), Box<dyn Error>> {
-        let mut w_outgoing_sender_opt = self.outgoing_sender.write().await;
-
-        if w_outgoing_sender_opt.is_none() {
-            let (tx, rx) = mpsc::channel(outgoing_buffer_size);
-            *w_outgoing_sender_opt = Some(Arc::new(tx));
-            let mut w_subscriber = subscriber.write().await;
-            w_subscriber.set_receiver(rx)
-        } else {
-            Err(Box::new(RnnError::PortBusy(format!(
-                "Port {} is busy",
-                self.id
-            ))))
-        }
-    }
-
-    pub async fn unsubscribe(&self) {
-        let mut w_outgoing_sender_opt = self.outgoing_sender.write().await;
-
-        if w_outgoing_sender_opt.is_some() {
-            *w_outgoing_sender_opt = None;
-        }
-    }
-
-    pub async fn is_subscribed(&self) -> bool {
-        self.outgoing_sender.read().await.is_some()
-    }
-
-    pub fn id(&self) -> Arc<String> {
-        self.id.clone()
-    }
-
-    pub async fn signal_hits_count(&self) -> usize {
-        self.signal_hits_counter.read().await.clone()
-    }
-
-    pub async fn inner_connect(
-        &mut self,
-        receiver: broadcast::Receiver<Signal<S>>,
-    ) -> Result<(), Box<dyn Error>> {
-        if self.inner_receiver.is_none() {
-            let inner_receiver = Arc::new(RwLock::new(receiver));
-            self.inner_receiver = Some(inner_receiver.clone());
-
-            let signal_hits_counter = self.signal_hits_counter.clone();
-            let outgoing_sender_opt = self.outgoing_sender.clone();
-
-            tokio::spawn(async move {
-                let mut w_inner_receiver = inner_receiver.write().await;
-                while let Ok(signal) = w_inner_receiver.recv().await {
-                    let mut w_signal_hits_counter = signal_hits_counter.write().await;
-                    *w_signal_hits_counter += 1;
-
-                    let r_outgoing_sender_opt = outgoing_sender_opt.read().await;
-                    if let Some(outgoing_sender) = r_outgoing_sender_opt.as_ref() {
-                        outgoing_sender.send(signal.intensity()).await; // TODO: handle result to notify about sending error
-                    }
-                }
-            });
-
+    /// Accepts connection from neuron, input port or to broadcast channel
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tokio::sync::broadcast;
+    /// use librunen::rnn::layouts::output_port::OutputPort;
+    ///
+    /// let mut port = OutputPort::<u8>::new(0);
+    ///
+    /// let (_tx, rx) = broadcast::channel(5);
+    /// assert!(port.connect(rx).is_ok());
+    /// assert!(port.is_connected());
+    /// ```
+    pub fn connect(&mut self, rx: broadcast::Receiver<Signal<S>>) -> Result<(), Box<dyn Error>> {
+        if self.receiver.is_none() {
+            self.receiver = Some(rx);
             Ok(())
         } else {
             Err(Box::new(RnnError::PortBusy(format!(
-                "Port {} is busy",
-                self.id
+                "Port {} already connected",
+                self.id()
             ))))
         }
     }
 
-    pub fn inner_disconnect(&mut self) {
-        if self.inner_receiver.is_some() {
-            self.inner_receiver = None;
+    /// Creates new receiver
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use librunen::rnn::layouts::output_port::OutputPort;
+    /// use tokio::sync::broadcast;
+    ///
+    /// let mut port = OutputPort::<u8>::new(0);
+    ///
+    /// let (_tx, rx) = broadcast::channel(5);
+    /// assert!(port.connect(rx).is_ok());
+    ///
+    /// assert!(port.resubscribe().is_some());
+    /// ```
+    pub fn resubscribe(&self) -> Option<broadcast::Receiver<Signal<S>>> {
+        if let Some(rx) = self.receiver.as_ref() {
+            Some(rx.resubscribe())
+        } else {
+            None
         }
     }
 
-    pub fn is_inner_connected(&self) -> bool {
-        self.inner_receiver.is_some()
+    /// Returns port id
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use librunen::rnn::layouts::output_port::OutputPort;
+    ///
+    /// let port = OutputPort::<u8>::new(5);
+    ///
+    /// assert_eq!(port.id(), "O_5");
+    /// ```
+    pub fn id(&self) -> &str {
+        self.id.as_str()
+    }
+
+    /// Returns signal hits counter value.
+    /// The counter takes into account the reception of signals by the port
+    /// if the port is connected to the output stream of values
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use std::error::Error;
+    /// use std::sync::Arc;
+    /// use chrono::Duration;
+    /// use librunen::rnn::layouts::output_port::OutputPort;
+    /// use librunen::rnn::common::signal::Signal;
+    /// use tokio::sync::{broadcast, Mutex};
+    /// use tokio_stream::StreamExt;
+    ///
+    /// #[tokio::main]
+    /// async fn main() -> Result<(), Box<dyn Error>> {
+    ///     let (tx, rx) = broadcast::channel(3);
+    ///     let mut port = OutputPort::new(2);
+    ///     port.connect(rx)?;
+    ///
+    ///     let port = Arc::new(Mutex::new(port));
+    ///
+    ///     let port_cloned = port.clone();
+    ///
+    ///     let handler_result = tokio::spawn(async move {
+    ///         let g_port = port.lock().await;
+    ///         let mut stream = Box::pin(g_port);
+    ///
+    ///         while let Some(msg) = stream.next().await {
+    ///             match msg {
+    ///                 Ok(msg) => assert!(msg > 0),
+    ///                 Err(_) => panic!("Error on signal processing"),
+    ///             }
+    ///         }
+    ///     });
+    ///
+    ///     for i in 1..=3 {
+    ///         tx.send(Signal::new(i))?;
+    ///     }
+    ///
+    ///     drop(tx);
+    ///     let (result,) = tokio::join!(handler_result);
+    ///     result?;
+    ///
+    ///     let g_port = port_cloned.lock().await;
+    ///     assert_eq!(g_port.signal_hits_count(), 3);
+    ///
+    ///     Ok(())
+    /// }
+    /// ```
+    pub fn signal_hits_count(&self) -> usize {
+        self.signal_hits_counter.load(Ordering::Relaxed)
+    }
+
+    /// Disconnects port from channel
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tokio::sync::broadcast;
+    /// use librunen::rnn::layouts::output_port::OutputPort;
+    ///
+    /// let mut port = OutputPort::<u8>::new(0);
+    ///
+    /// let (_tx, rx) = broadcast::channel(5);
+    /// assert!(port.connect(rx).is_ok());
+    ///
+    /// port.disconnect();
+    /// assert!(!port.is_connected());
+    /// ```
+    pub fn disconnect(&mut self) {
+        if self.receiver.is_some() {
+            self.receiver = None;
+        }
+    }
+
+    /// Checkes if port has incoming connection
+    pub fn is_connected(&self) -> bool {
+        self.receiver.is_some()
+    }
+}
+
+impl<S> Stream for OutputPort<S>
+where
+    S: Arithmetic,
+{
+    type Item = Result<S, broadcast::error::RecvError>;
+
+    fn poll_next(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
+        let receiver = match self.receiver.as_mut() {
+            Some(rx) => rx,
+            None => return Poll::Ready(None),
+        };
+
+        match receiver.try_recv() {
+            Ok(item) => {
+                self.signal_hits_counter.fetch_add(1, Ordering::Release);
+                Poll::Ready(Some(Ok(item.intensity())))
+            }
+            Err(broadcast::error::TryRecvError::Empty) => {
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            }
+            Err(broadcast::error::TryRecvError::Lagged(skipped)) => {
+                Poll::Ready(Some(Err(broadcast::error::RecvError::Lagged(skipped))))
+            }
+            Err(broadcast::error::TryRecvError::Closed) => Poll::Ready(None),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-
-    use std::{pin::Pin, task::Poll, time::Duration};
-
-    use tokio_stream::{Stream, StreamExt};
+    use tokio_stream::StreamExt;
+    use tokio::sync::Mutex;
 
     use super::*;
 
-    struct ConcreteSubscriber<S>
-    where
+    async fn process_message<S>(
+        port: OutputPort<S>,
+        results: Arc<Mutex<Vec<Result<S, broadcast::error::RecvError>>>>,
+    ) where
         S: Arithmetic,
     {
-        receiver: Option<mpsc::Receiver<S>>,
-    }
-
-    impl<S> Subscriber<S> for ConcreteSubscriber<S>
-    where
-        S: Arithmetic,
-    {
-        fn set_receiver(&mut self, receiver: mpsc::Receiver<S>) -> Result<(), Box<dyn Error>> {
-            if self.receiver.is_none() {
-                self.receiver = Some(receiver);
-                Ok(())
-            } else {
-                Err(Box::new(std::fmt::Error::default()) as Box<dyn Error>)
-            }
-        }
-    }
-
-    impl<S> Stream for ConcreteSubscriber<S>
-    where
-        S: Arithmetic,
-    {
-        type Item = S;
-
-        fn poll_next(
-            mut self: std::pin::Pin<&mut Self>,
-            cx: &mut std::task::Context<'_>,
-        ) -> std::task::Poll<Option<Self::Item>> {
-            if let Some(receiver) = &mut self.receiver {
-                Pin::new(receiver).poll_recv(cx)
-            } else {
-                Poll::Ready(None)
+        let mut stream = Box::pin(port);
+        let mut w_results = results.lock().await;
+        while let Some(msg) = stream.next().await {
+            match msg {
+                Ok(msg) => w_results.push(Ok(msg)),
+                Err(err) => w_results.push(Err(err)),
             }
         }
     }
@@ -192,23 +260,23 @@ mod tests {
     fn test_port_id_getter() {
         let port = OutputPort::<u8>::new(1);
 
-        assert_eq!(port.id().as_str(), "O_1");
+        assert_eq!(port.id(), "O_1");
     }
 
     #[test]
-    fn for_new_port_is_inner_connected_should_returns_false() {
+    fn for_new_port_is_connected_should_returns_false() {
         let port = OutputPort::<u8>::new(1);
 
-        assert!(!port.is_inner_connected());
+        assert!(!port.is_connected());
     }
 
-    #[tokio::test]
-    async fn test_output_port_can_inner_connect_channel() -> Result<(), Box<dyn Error>> {
+    #[test]
+    fn test_output_port_can_connect_channel() -> Result<(), Box<dyn Error>> {
         let (_tx, rx) = broadcast::channel::<Signal<u8>>(5);
         let mut output_port = OutputPort::<u8>::new(0);
 
-        output_port.inner_connect(rx).await?;
-        assert!(output_port.is_inner_connected());
+        output_port.connect(rx)?;
+        assert!(output_port.is_connected());
 
         Ok(())
     }
@@ -219,13 +287,35 @@ mod tests {
         let (tx, rx) = broadcast::channel::<Signal<u8>>(5);
         let mut output_port = OutputPort::<u8>::new(0);
 
-        output_port.inner_connect(rx).await?;
+        output_port.connect(rx)?;
+
+        let output_port = Arc::new(Mutex::new(output_port));
+
+        let port_cloned = output_port.clone();
+
+        let handler_result = tokio::spawn(async move {
+            let g_port = output_port.lock().await;
+            let mut stream = Box::pin(g_port);
+
+            while let Some(msg) = stream.next().await {
+                match msg {
+                    Ok(msg) => assert_eq!(msg, 42),
+                    Err(_) => panic!("Error on signal processing"),
+                }
+            }
+        });
 
         tx.send(Signal::new(42))?;
+        tx.send(Signal::new(42))?;
 
-        tokio::time::sleep(Duration::from_micros(1)).await;
+        drop(tx);
+        let (result,) = tokio::join!(handler_result);
 
-        assert_eq!(output_port.signal_hits_count().await, 1);
+        assert!(result.is_ok());
+
+
+        let g_port = port_cloned.lock().await;
+        assert_eq!(g_port.signal_hits_count(), 2);
 
         Ok(())
     }
@@ -234,60 +324,25 @@ mod tests {
     async fn consumer_can_subscribe_to_output_port() -> Result<(), Box<dyn Error>> {
         let (tx, rx) = broadcast::channel::<Signal<u8>>(5);
         let mut output_port = OutputPort::<u8>::new(0);
-        let concrete_subscriber = ConcreteSubscriber { receiver: None };
-        let concrete_subscriber = Arc::new(RwLock::new(concrete_subscriber));
 
-        output_port.inner_connect(rx).await?;
+        let results = Arc::new(Mutex::new(vec![]));
+        output_port.connect(rx)?;
 
-        output_port
-            .subscribe(concrete_subscriber.clone(), 5)
-            .await?;
-
-        tokio::spawn(async move {
-            let mut w_concrete_subscriber = concrete_subscriber.write().await;
-            while let Some(msg) = w_concrete_subscriber.next().await {
-                assert!(msg > 0);
-                if msg == 5 {
-                    break;
-                }
-            }
-        });
+        let consumer = tokio::spawn(process_message(output_port, results.clone()));
 
         for i in 1_u8..=5_u8 {
             let result = tx.send(Signal::new(i))?;
             assert!(result > 0);
         }
+        drop(tx);
+
+        let _ = tokio::spawn(consumer);
+
+        let g_results = results.lock().await;
+        for result in g_results.iter() {
+            assert!(result.is_ok());
+        }
+
         Ok(())
-    }
-
-    #[tokio::test]
-    async fn for_new_output_port_is_subscribed_returns_false() {
-        let port = OutputPort::<u8>::new(0);
-
-        assert!(!port.is_subscribed().await);
-    }
-
-    #[tokio::test]
-    async fn when_consumer_was_subscribed_to_output_port_is_subscribed_should_return_true() {
-        let port = OutputPort::<i64>::new(0);
-
-        let concrete_subscriber = Arc::new(RwLock::new(ConcreteSubscriber { receiver: None }));
-
-        port.subscribe(concrete_subscriber, 5).await;
-
-        assert!(port.is_subscribed().await);
-    }
-
-    #[tokio::test]
-    async fn test_unsubscribe_subscribed_consumer_from_port() {
-        let port = OutputPort::<usize>::new(0);
-
-        let consumer = Arc::new(RwLock::new(ConcreteSubscriber { receiver: None }));
-
-        port.subscribe(consumer, 2).await;
-
-        port.unsubscribe().await;
-
-        assert!(!port.is_subscribed().await);
     }
 }
